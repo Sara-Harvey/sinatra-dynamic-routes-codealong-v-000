14f20d7c9f909e4feee6428dba4b018a56b62571
Sara Harvey <saraharveynyc@gmail.com>
2019-08-06 16:53:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,10 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}!"
   end
 
-
+  get '/multiply/:num1/:num2' do 
+     @num_1 = params[:num1].to_i
+     @num_2 = params[:num2].to_i
+      "#{@num_1 * @num_2}"
+  end
 
 end
